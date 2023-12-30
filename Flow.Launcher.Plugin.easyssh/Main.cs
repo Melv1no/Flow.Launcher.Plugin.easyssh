9c31e4a8c9250cc0ed431bd4aafcaa332f5ea1df
Melvin OLIVET <olivet.melvin@gmail.com>
2023-12-30 01:53:20 +0100

    Profile feature experimental

--- Flow.Launcher.Plugin.easyssh/Main.cs
+++ Flow.Launcher.Plugin.easyssh/Main.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using Flow.Launcher.Plugin;
+using System.Linq;
+
 
 namespace Flow.Launcher.Plugin.easyssh
 {
     public class EasySSH : IPlugin
     {
         private PluginInitContext _context;
+        private ProfileManager _profile;
 
         public void Init(PluginInitContext context)
         {
@@ -15,36 +19,120 @@ namespace Flow.Launcher.Plugin.easyssh
             {
                 return;
             }
+
             _context = context;
+            _profile = new ProfileManager(_context.CurrentPluginMetadata.PluginDirectory + "\\profiles.json");
         }
 
         public List<Result> Query(Query query)
-        { 
-            var result = new Result
+        {
+            var easyssh_main_cmd = new Result();
+            string subtitle = "<add,remove,profiles>/<direct ssh command>";
+            String[] command = query.Search.Split(' ');
+
+            switch (command[0])
             {
-                Title = "EasySSH",
-                SubTitle = $"connect to: {query.Search}",
-                Action = c =>
-                {        
-                    new Process
+                case "add":
+                    if (command.Length >= 3 && !string.IsNullOrEmpty(command[1]))
                     {
-                        StartInfo = new ProcessStartInfo
+                        string sshCommand = string.Join(" ", command.Skip(2));
+                        subtitle = sshCommand;
+                        easyssh_main_cmd.Action = context =>
                         {
-                            FileName = "ssh.exe",
-                            Arguments = query.Search,
-                            RedirectStandardInput = false,
-                            RedirectStandardOutput = false,
-                            RedirectStandardError = false,
-                            UseShellExecute = true,
-                            CreateNoWindow = false
-                        }
-                    }.Start();
-                    
-                    return true;
-                },
-                IcoPath = "Images/app.png"
-            };
-            return new List<Result>() {result};
+                            _profile.addProfile(command[1], sshCommand);
+                            return true;
+                        };
+                    }
+                    else
+                    {
+                        subtitle = "add <profile name> <ssh command>";
+                    }
+
+                    break;
+                case "remove":
+                    subtitle = "choose the profile you want remove";
+                    List<Result> remove_result = new List<Result>();
+                    remove_result.Add(new Result
+                    {
+                        Title = "Choose the profile to remove",
+                    });
+                    foreach (Profile profile in _profile.getProfiles())
+                    {
+                        remove_result.Add(new Result
+                        {
+                            Title = profile.Name,
+                            SubTitle = profile.Command,
+                            IcoPath = "Images/app-red.png",
+                            Action = context =>
+                            {
+                                _profile.removeProfile(profile.Id);
+                                return true;
+                            }
+                        });
+                    }
+                    return remove_result;
+                case "profiles":
+                    subtitle = "profiles";
+                    List<Result> profiles_result = new List<Result>();
+                    foreach (Profile profile in _profile.getProfiles())
+                    {
+                        profiles_result.Add(new Result
+                        {
+                            Title = profile.Name,
+                            SubTitle = profile.Command,
+                            IcoPath = "Images/app-green.png",
+                            Action = context =>
+                            {
+                                new Process
+                                {
+                                    StartInfo = new ProcessStartInfo
+                                    {
+                                        FileName = "ssh.exe",
+                                        Arguments = profile.Command,
+                                        RedirectStandardInput = false,
+                                        RedirectStandardOutput = false,
+                                        RedirectStandardError = false,
+                                        UseShellExecute = true,
+                                        CreateNoWindow = false
+                                    }
+                                }.Start();
+                                return true;
+                            }
+                        });
+                    }
+
+                    return profiles_result;
+                case "d":
+                    subtitle = $"direct connect to:{query.Search.Substring(1)}";
+                    string commands = string.Join(" ", command.Skip(1));
+                    easyssh_main_cmd.Action = context =>
+                    {
+                        new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "ssh.exe",
+                                Arguments = commands,
+                                RedirectStandardInput = false,
+                                RedirectStandardOutput = false,
+                                RedirectStandardError = false,
+                                UseShellExecute = true,
+                                CreateNoWindow = false
+                            }
+                        }.Start();
+
+                        return true;
+                    };
+                    break;
+                default:
+                    subtitle = "usage: add ; remove ; profiles ; d";
+                    break;
+            }
+
+            easyssh_main_cmd.Title = "EasySSH";
+            easyssh_main_cmd.SubTitle = $"{subtitle}";
+            easyssh_main_cmd.IcoPath = "Images/app.png";
+            return new List<Result>() { easyssh_main_cmd };
         }
     }
 }
---
+++ Flow.Launcher.Plugin.easyssh/Profile.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using YamlDotNet.Serialization;
+
+public class Profile
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Command { get; set; }
+
+    public Profile( int id, string name, string command)
+    {
+        Id = id;
+        Name = name;
+        Command = command;
+    }
+}
+
+public class ProfileManager
+{
+    private string _path;
+    private string _file;
+    
+    public ProfileManager(String path)
+    {
+        _path = path;
+        
+        if (!isDatabaseCreated())
+        {
+            File.WriteAllText(_path,"{}");
+        }
+        _file = File.ReadAllText(_path);
+    }
+
+    public bool isDatabaseCreated()
+    {
+        return File.Exists(_path);
+    }
+
+    public List<Profile> getProfiles()
+    {
+        List<Profile> profiles = new List<Profile>();
+        JObject jsonObject = JObject.Parse(_file);
+
+        foreach (var item in jsonObject)
+        {
+            string key = item.Key;
+            JObject data = item.Value as JObject;
+
+            string id = data["Id"].ToString();
+            string profile = data["Name"].ToString();
+            string command = data["Command"].ToString();
+            profiles.Add(new Profile(int.Parse(key),profile,command ));
+        }
+
+        return profiles;
+    }
+    public void addProfile(string name, string command)
+    {
+        JObject jsonObject = JObject.Parse(_file);
+
+        int lastId = jsonObject.Properties().Any() ? int.Parse(jsonObject.Properties().Last().Name) : 0;
+
+        int newId = lastId + 1;
+
+        Profile newProfile = new Profile(newId, name, command);
+
+        jsonObject[newId.ToString()] = JObject.FromObject(newProfile);
+
+        _file = jsonObject.ToString();
+        File.WriteAllText(_path, _file);
+    }
+    
+    public void removeProfile(int id)
+    {
+        JObject jsonObject = JObject.Parse(_file);
+
+        if (jsonObject.ContainsKey(id.ToString()))
+        {
+            jsonObject.Remove(id.ToString());
+            _file = jsonObject.ToString();
+            File.WriteAllText(_path, _file);
+        }
+    }
+}
